e498b0c7f3867958f0dbdbc01dfe8b2225917736
lingyunyumo <lingyunyumo@gmail.com>
2013-10-24 19:54:49 +0800

    Signed-off-by: lingyunyumo <lingyunyumo@gmail.com>

---
+++ local_run.py
@@ -0,0 +1,16 @@
+#-*- coding:utf-8 -*-
+
+from __future__ import unicode_literals, print_function
+
+
+#import pylinden.cli
+
+#pylinden.cli.CLI().cmdloop()
+
+from pylinden.generator import Generator
+
+gen = Generator()
+gen.generate()
+
+
+
